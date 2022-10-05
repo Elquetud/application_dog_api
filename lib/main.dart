487d48b58e615efb062d1af9e582c2346797f6a8
Elquetud <jesusromeroarmenta@outlook.com>
2022-10-05 01:03:19 -0600

    dependencia http y solicitud dog y modelo

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
+import 'package:application_dog_api/models/Dog.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+
 
 void main() {
   runApp(const MyApp());
@@ -31,9 +37,26 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-
-
   class _MyHomePageState extends State<MyHomePage> {
+
+    late Future<List<DogList>> _listadoDogs;
+
+    Future<List<DogList>> _getDogs() async{
+      final response =await http.get("https://dog.ceo/api/breeds/list/all");
+      
+      if(response.statusCode ==200 ){
+       print(response.body);
+      }else{
+        throw Exception("Fallo la conexión");
+      }
+      return _getDogs();
+    }
+
+  @override
+  void initState() {
+    super.initState();
+    _getDogs();
+  }
 
   @override
   Widget build(BuildContext context) {
