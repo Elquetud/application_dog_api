91c22434d2f3b1f59db3185070263dbd41a607d7
Elquetud <jesusromeroarmenta@outlook.com>
2022-10-04 18:18:02 -0600

    Limpieza del main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Dog Api Aplication',
+      title: 'Dog Api Aplication.',
       theme: ThemeData(
        
         primarySwatch: Colors.blue,
